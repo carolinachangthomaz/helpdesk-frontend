8a836a79de283477e33c5cfe294f3a100d056005
Carolina Thomaz <carol.com.jp@gmail.com>
2018-05-29 22:17:42 -0300

    Service - Ticket

---
+++ src/app/model/ticket.model.ts
@@ -0,0 +1,16 @@
+import { User } from "./user.model";
+
+export class Ticket{
+    constructor(
+       public id: string,
+       public numero: number,
+       public titulo: string,
+       public status: string,
+       public prioridade: string,
+       public image: string,
+       public user: User,
+       public usuarioDesignado: User,
+       public data: string,
+       public listStatusAlteracao: Array<string>
+    ){}
+}
---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,50 @@
+import { Ticket } from './../model/ticket.model';
+import { Injectable, NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HELP_DESK_API } from './helpdesk.api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  constructor(private http: HttpClient) { }
+
+  createOrUpdate(ticket: Ticket){
+    if(ticket.id != null && ticket.id != ''){
+      return this.http.put('${HELP_DESK_API}/tickets', ticket);
+    }else{
+      ticket.id = null;
+      ticket.status = 'Novo';
+      return this.http.post('${HELP_DESK_API}/tickets', ticket);
+    }
+  }
+
+  findAll(page: number, count: number){
+    return this.http.get('${HELP_DESK_API}/tickets/${page}/${count}');
+  }
+
+  findById(id: string){
+    return this.http.get('${HELP_DESK_API}/tickets/${id}');
+  }
+
+  delete(id: string){
+    return this.http.delete('${HELP_DESK_API}/tickets/${id}');
+  }
+
+  findByParans(page: number, count: number,designados:boolean,ticket: Ticket){
+    ticket.numero = ticket.numero == null ? 0 : ticket.numero;
+    ticket.titulo = ticket.titulo == '' ? 'uninformed' : ticket.titulo;
+    ticket.status = ticket.status == '' ? 'uninformed' : ticket.status;
+    ticket.prioridade = ticket.prioridade == '' ? 'uninformed' : ticket.prioridade;
+    return this.http.get('${HELP_DESK_API}/tickets/${page}/${count}/${ticket.numero}/${ticket.titulo}/${ticket.status}/${ticket.prioridade}/${designado}');
+  }
+
+  alterarStatus(status: string, ticket: Ticket){
+    return this.http.put('${HELP_DESK_API}/tickets/${ticket.id}/${status}', ticket);
+  }
+
+  sumario(){
+    return this.http.get('${HELP_DESK_API}/tickets/sumarios');
+  }
+}
